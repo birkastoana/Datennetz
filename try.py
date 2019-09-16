6f55e59031f6f57d1620f0e5af3187acb52fbadd
Christian <dc3173@mci4me.at>
2019-09-16 21:28:47 +0200

    request not successfull

--- try.py
+++ try.py
@@ -16,23 +16,27 @@ try:
     urlElements = urlparse(sys.argv[1])
 
     print('urlElements', urlElements)
-    print('scheme: ', urlElements[2])
+
 
 
     # Wir erzeugen einen Socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Wir verbinden uns mit dem Server (Host: google.at, Port: 80)
-    sock.connect(("www.google.at", 80))
+    sock.connect((urlElements[1], 80))
 
     # Wir senden eine Anfrage an den Server
     # Python3 verwendet intern Unicode-Strings, Ã¼ber das Netzwerk kÃ¶nnen wir nur Byte-Strings schicken
     # daher mÃ¼ssen wir Python-String mit encode('<codec>') in einen Byte-String umwandeln
-    sock.send('GET / HTTP/1.1\r\nhost: google.at\r\n\r\n'.encode("utf-8"))
+
+    requestString = 'GET / HTTP/1.0\r\nhost: ' + 'google.at' + '\r\n\r\n'
+    print('requeststring ', requestString.encode("utf-8"))
+    # sock.send(requestString.encode("utf-8"))
+    sock.send('GET / HTTP/1.1\r\nhost: www.google.at\r\n\r\n'.encode("utf-8"))
 
     # Wir empfangen die Antwort vom Server
     # Als BuffergrÃ¶ÃŸe verwenden wir 2048, wenn die Nachricht grÃ¶ÃŸer ist, mÃ¼ssen wir recv() mehrmals aufrufen
-    response = sock.recv(2048)
+    response = sock.recv(4048)
 
     # Wir geben die Antwort aus
     # Um einen Byte-String in einen Unicode-String umzuwandeln, mÃ¼ssen wir decode() aufrufen
