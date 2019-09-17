a891ae751f6e42de73eb339121eac87ce4ce8418
Christian <dc3173@mci4me.at>
2019-09-17 22:08:06 +0200

    html 1.1 request works

--- try.py
+++ try.py
@@ -12,43 +12,66 @@ import re
 
 try:
     if  len(sys.argv) != 3 :
+        print(sys.argv)
         raise Exception("Invalid number of Arguments")
 
     urlElements = urlparse(sys.argv[1])
+    print ("urlelements: " ,  urlElements)
 
-    print('urlElements', urlElements)
+    # verwende die variablen, weil die urlElements spaeter ggf ueberschrieben werden!
+    path = urlElements[2]
+    params = urlElements[3]
+    query = urlElements[4]
+
+    loopFlag = 1
+    while loopFlag:
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((urlElements[1], 80))
+        print("++++++++++++++++++++++")
+        print(urlElements[1])
+        print("++++++++++++++++++++++")
+
+        requestString = 'GET '  + path
+        if len(query) != 0: requestString = requestString + '?' + query
+        requestString = requestString + " HTTP/1.1\r\nHost: " + urlElements[1] +"\r\n\r\n"
+
+        print("~~~~~~~~~~~~~~~")
+        print('requeststring ', requestString.encode("utf-8"))
+
+        print("~~~~~~~~~~~~~~~")
+        sock.send(requestString.encode("utf-8"))
+        response = sock.recv(4048)
+
+        response = response.split(b"\r\n\r\n")
+        head = response[0]
+        body = response[1]
+        print(head)
+
+        requestStatus = re.match(b"HTTP/\d*\.\d* (\d+)", head)
+        statusCode = requestStatus[1].decode()
+
+        if statusCode == "200":
+            print("********** entered status 200 ***********")
+            loopFlag = 0
+        elif statusCode[0] == "3":
+            print(statusCode[0])
+            print("********** entered status 3xx ***********")
+            newAdress = re.match(".*\sLocation: (.*)\s.*", head.decode())
+            urlElements = urlparse(newAdress[1])
+        elif statusCode[0] == "4" or statusCode[0] == "5":
+            print("********** entered error  ***********")
+            error = re.match("HTTP/\d.\d (.*)\s.*", head.decode())
+            print(error[1])
+            raise Exception(error[1] + requestString)
 
 
 
-    # Wir erzeugen einen Socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Wir verbinden uns mit dem Server (Host: google.at, Port: 80)
-    sock.connect((urlElements[1], 80))
 
-    # Wir senden eine Anfrage an den Server
-    # Python3 verwendet intern Unicode-Strings, Ã¼ber das Netzwerk kÃ¶nnen wir nur Byte-Strings schicken
-    # daher mÃ¼ssen wir Python-String mit encode('<codec>') in einen Byte-String umwandeln
+        sock.close()
 
-    requestString = 'GET / HTTP/1.0\r\nhost: ' + urlElements[1] + '\r\n\r\n'
-    print('requeststring ', requestString.encode("utf-8"))
-    sock.send(requestString.encode("utf-8"))
-    # sock.send('GET / HTTP/1.1\r\nhost: www.google.at\r\n\r\n'.encode("utf-8"))
 
-    # Wir empfangen die Antwort vom Server
-    # Als BuffergrÃ¶ÃŸe verwenden wir 2048, wenn die Nachricht grÃ¶ÃŸer ist, mÃ¼ssen wir recv() mehrmals aufrufen
-    response = sock.recv(4048)
-
-    # Wir geben die Antwort aus
-    # Um einen Byte-String in einen Unicode-String umzuwandeln, mÃ¼ssen wir decode() aufrufen
-    # print(response.decode())
-    requestStatus = re.match(response, "HTTP/\d\.\d (\d+)")
-    print(requestStatus)
-
-    # print(re.match(response, "HTTP/(\d\.\d) (\d+) (\w+)\\r\\n.*Cache-Control:"))
-
-    # Nicht vergessen alle Ressourcen wieder zu schlieÃŸen
-    sock.close()
 
 except:
     print("Ein Fehler ist aufgetreten")
